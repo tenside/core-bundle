3fabf9a231b21e89c3f3f355fe811b51ad39a30c
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-04-01 19:59:12 +0200

    Fix author validation again

--- src/DependencyInjection/Compiler/AddTaskFactoryPass.php
+++ src/DependencyInjection/Compiler/AddTaskFactoryPass.php
@@ -11,6 +11,7 @@
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    tenside/core-bundle
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Yanick Witschi <yanick.witschi@terminal42.ch>
  * @copyright  2015 Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @license    https://github.com/tenside/core-bundle/blob/master/LICENSE MIT
